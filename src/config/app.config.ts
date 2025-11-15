1a53ccd94d50f443825810519362c440b4f63e04
amubhya <43973623+ridwanmubarok@users.noreply.github.com>
2025-11-15 16:19:07 +0700

    add: nixpack

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,4 +1,4 @@
 export default () => ({
   APP_NAME: process.env.APP_NAME || 'My App',
-  APP_PORT: process.env.APP_PORT || 3000,
+  APP_PORT: Number(process.env.PORT || process.env.APP_PORT || 3000),
 });
